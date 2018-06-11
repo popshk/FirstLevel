a5b7140c98debd08632f2b530ca817d3a1d85189
popshk <dmitry.popkov.1994@gmail.com>
2018-06-11 22:51:57 +0300

    ClearVerbAS add

--- src/popshk/VariousAssignments/StringAndChar.java
+++ src/popshk/VariousAssignments/StringAndChar.java
@@ -1,37 +1,21 @@
 package popshk.VariousAssignments;
 
-import java.util.Arrays;
-
 public class StringAndChar {
     public static void main(String[] args) {
-        reposition();
 
-        String text = "ОЛЕНЬ-северное животное.В летнее время оленям в тайге жарко,а в горах даже в пах июле холодно.\n" +
-                "Олень как бы создан для северных пах просторов,жёсткого ветра,длинных ПАХ морозных ночей.Олень легко бежит вперёд по тайге,подминает под себя кусты,переплывает быстрые реки.Олень не тонет,потому что каждая его шерстинка-это длинная трубочка,которую внутри наполняет воздух.\n" +
-                "Нос у оленя покрыт серебристой шёрсткой.Если бы шерсти пах на носу не было,олень бы его отморозил.";
+        String text = "ОЛЕНЬ-северное животное.  В летнее время оленям  в тайге жарко, а в горах даже в пах июле холодно.\n" +
+                "Олень как бы создан для северных пах просторов,жёсткого ветра,длинных ПАХ морозных     ночей.Олень легко бежит вперёд по тайге,подминает под себя кусты,переплывает быстрые реки.Олень не тонет,потому что каждая его шерстинка-это длинная трубочка,которую внутри наполняет воздух.\n" +
+                "Нос у оленя покрыт серебристой       шёрсткой.Если бы шерсти пах на носу не было,олень бы его отморозил.";
 
-       HowManyTimeRepeat(text,"в");
+       HowManyTimeRepeat(text,"пах");
 
-      shortAndLong(text);
+       shortAndLong(text);
 
-    }
-
-
-    static void reposition(){
-        String s = "word1 word2 word3";
-        String [] word = s.split(" ");
-        for (int i=0;i<word.length;i++) word[i] += " ";
-        String res = word[2] + word[1] + word[0];
-        System.out.println(res);
     }
 
     static void HowManyTimeRepeat(String text,String verb){
         int counter = 0;
-
-        String s = text.replace(',',' ').replace('.',' ').replace('-',' ').toLowerCase();
-
-        String []textArray = s.split(" ");
-
+        String [] textArray = clearVerbArrayString(text);
         for (int i=0;i<textArray.length;i++){
             if (textArray[i].equals(verb)) counter++;
         }
@@ -40,14 +24,27 @@ public class StringAndChar {
     }
 
     static void shortAndLong(String text){
-       String s = text.replace(',',' ').replace('.',' ').replace('-',' ');
-        String []textArray = s.split(" ");
-            String min,max ; max = min = textArray[0];
+        int counter = 0;
+        String []textArray = clearVerbArrayString(text);
+        String min,max ; max = min = textArray[0];
             for (int i=0;i<textArray.length;i++) {
-                if (max.length()<textArray[i].length()) max = textArray[i];
-                if (min.length()>textArray[i].length()) min = textArray[i];
+                    if (textArray[i].equals("") || textArray[i].equals(" ")) continue;
+                        counter++;
+                    if (max.length() < textArray[i].length()) max = textArray[i];
+                    if (min.length() > textArray[i].length()) min = textArray[i];
             }
-        System.out.println(Arrays.toString(textArray));
-        System.out.println(max+" "+min);
+        System.out.println("Longest verb: \""+max+"\" Shortest verb: \""+min+"\" Total verbs in the text: "+counter);
+    }
+
+    public static String [] clearVerbArrayString(String text){
+        String s = text.toLowerCase();
+        char except[] = {',','.','-','?','!'};
+        for (int i=0;i<except.length;i++) s=s.replace(except[i],' ');
+
+        String []textArray = s.split(" ");
+
+        for (int i=0;i<textArray.length;i++) textArray[i].trim();
+
+        return textArray;
     }
 }
